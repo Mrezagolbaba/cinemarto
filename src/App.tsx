53ff8d2f79d391c21bcdc91ac028cfba12e90f77
Mohamadrez@ <m.reza.golbaba@gmail.com>
2024-08-07 00:34:53 +0300

    add router

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './containers/Home';
+import MovieDetails from './components/details';
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
