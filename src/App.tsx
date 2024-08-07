2805e70a2ea173760536b89faf02136008fc5a58
Mohamadrez@ <m.reza.golbaba@gmail.com>
2024-08-07 22:18:03 +0300

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { store } from './store';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './containers/Home';
 import MovieDetails from './components/details';
+import './App.css'
 
 const App: React.FC = () => {
   return (
