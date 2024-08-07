0bb19182d4af6a95dfa6dc9ca6bbef996c46f62b
Mohamadrez@ <m.reza.golbaba@gmail.com>
2024-08-07 22:17:02 +0300

    create tags component

--- src/components/tags/index.tsx
+++ src/components/tags/index.tsx
@@ -1,19 +1,24 @@
-const Tags =()=>{
+import styles from './styles.module.scss';
 
-    const tags = ['Now Playing', 'Popular', 'Top Rated', 'Upcoming']
-    const handleClick = (tag:string) => {
-        console.log(tag)
-    }
-    return(
-        <div>
-            <ul>
+const Tags = () => {
+    const tags = ['Now Playing', 'Popular', 'Top Rated', 'Upcoming'];
+
+    const handleClick = (tag: string) => {
+        console.log(tag);
+        // Add logic to set the query based on the clicked tag
+    };
+
+    return (
+        <div className={styles.tagsContainer}>
+            <ul className={styles.tagsList}>
                 {tags.map((tag, index) => (
-                    <li key={index} onClick={() => handleClick(tag)}>
+                    <li key={index} className={styles.tagItem} onClick={() => handleClick(tag)}>
                         {tag}
                     </li>
                 ))}
             </ul>
         </div>
-    )
-}
+    );
+};
+
 export default Tags;
