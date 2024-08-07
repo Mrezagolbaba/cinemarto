32b77acf48e32334aefaf14527ffdf2f66450f86
Mohamadrez@ <m.reza.golbaba@gmail.com>
2024-08-07 22:15:51 +0300

    update details page

--- src/components/details/index.tsx
+++ src/components/details/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useGetMovieDetailsQuery } from '../../services/movies';
-import s from './styles.module.scss';
+import styles from './styles.module.scss';
+import Rating from '../Rating';
 
 const MovieDetails: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -11,12 +12,14 @@ const MovieDetails: React.FC = () => {
   if (error) return <div>Error: {error.toString()}</div>;
 
   return (
-    <div className={s['movie-details']}>
-      <h1>{movie.title}</h1>
+    <div className={styles.container}>
       <img src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-      <p>{movie.overview}</p>
-      <p>Release Date: {movie.release_date}</p>
-      <p>Rating: {movie.vote_average}</p>
+      <div className={styles.details}>
+        <h1>{movie.title}</h1>
+        <p>{movie.overview}</p>
+        <p>Release Date: {movie.release_date}</p>
+        <Rating totalStars={movie.vote_average} />
+      </div>
     </div>
   );
 };
