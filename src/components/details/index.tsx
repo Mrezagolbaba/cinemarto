594a0ef14f7e5599989c993d7bfa8a9194860c37
Mohamadrez@ <m.reza.golbaba@gmail.com>
2024-08-09 23:46:06 +0300

    update details page

--- src/components/details/index.tsx
+++ src/components/details/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useGetMovieDetailsQuery } from '../../services/movies';
+import { useGetMovieDetailsQuery } from 'src/services/movies';
 import styles from './styles.module.scss';
-import Rating from '../Rating';
+import GoBack from 'src/assets/arrowRight.svg';
+import Rating from 'src/components/Rating';
 
 const MovieDetails: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -10,16 +11,24 @@ const MovieDetails: React.FC = () => {
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.toString()}</div>;
-
+  const handleGoBack = () => {
+    window.history.back();
+  }
   return (
     <div className={styles.container}>
-      <img src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-      <div className={styles.details}>
-        <h1>{movie.title}</h1>
-        <p>{movie.overview}</p>
-        <p>Release Date: {movie.release_date}</p>
-        <Rating totalStars={movie.vote_average} />
+      <div className={styles.header}>
+        <img src={GoBack} alt="Go Back" onClick={handleGoBack} />
       </div>
+      <div className={styles.content}>
+        <img src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+        <div className={styles.details}>
+          <h1>{movie.title}</h1>
+          <p>{movie.overview}</p>
+          <p>Release Date: {movie.release_date}</p>
+          <Rating totalStars={movie.vote_average} />
+        </div>
+      </div>
+
     </div>
   );
 };
